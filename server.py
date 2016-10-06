b31cca77db5756364c0f0e3cfdd9917b9b5bca6e
Becky Smith <rebkwok@gmail.com>
2016-10-06 11:14:50 +0100

    Update to Python 3 and refactor

--- server.py
+++ server.py
@@ -2,73 +2,45 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template, request, redirect
-from urllib import urlencode
+from urllib.parse import urlencode
 
 import sockgen
 
 
 app = Flask(__name__)
 
+
 def form_input(request):
 
-    units = request.args.get('units', 'inches')
-    error = False
+    # result dict with each parameter as a tuple of (value, error)
+    # units is a choice field, so no error option
+    result = {
+        'units': request.args.get('units', 'inches'),
+        'stitches': request.args.get('stitches', 24),
+        'rows': request.args.get('rows', 30),
+        'circum': request.args.get('circum', 7),
+        'foot_length': request.args.get('foot_length', 8),
+        'ease': request.args.get('ease', -8),
+    }
 
-    try:
-        stitches = float(request.args.get('stitches', 24))
-        st_color = "#FFFFFF"
-    except ValueError:
-        stitches = request.args.get('stitches')
-        st_color = "#FA5858"
-        error = True
+    errors = []
 
-    try:
-        rows = float(request.args.get('rows', 30))
-        r_color = "#FFFFFF"
-    except ValueError:
-        rows = request.args.get('rows')
-        r_color = "#FA5858"
-        error = True
+    for param in ['stitches', 'rows', 'circum', 'foot_length', 'ease']:
+        try:
+            result.update({param: float(result[param])})
+        except ValueError:
+            errors.append(param)
 
-    try:
-        circum = float(request.args.get('circum', 7))
-        c_color = "#FFFFFF"
-    except ValueError:
-        c_color = "#FA5858"
-        circum = request.args.get('circum')
-        error = True
-
-    try:
-        foot_length = float(request.args.get('foot_length', 8))
-        fl_color = "#FFFFFF"
-    except ValueError:
-        fl_color = "#FA5858"
-        foot_length = request.args.get('foot_length')
-        error = True
-
-    try:
-        ease = float(request.args.get('ease', -8))
-        e_color = "#FFFFFF"
-    except ValueError:
-        ease = request.args.get('ease')
-        e_color = "#FA5858"
-        error = True
-
-    result = [st_color, r_color, c_color, fl_color, e_color, units, stitches, rows, circum, foot_length, ease, error]
-    return result
+    return result, errors
 
 
 @app.route('/')
 def form():
+    input_fields, error_list = form_input(request)
+    return render_template(
+        'form.html', data=input_fields, error_list=error_list
+    )
 
-    input_fields = form_input(request)
-
-    result = render_template('form.html', st_color=input_fields[0], r_color=input_fields[1], c_color=input_fields[2],
-                             fl_color=input_fields[3], e_color=input_fields[4], units=input_fields[5],
-                             stitches=input_fields[6], rows=input_fields[7], circum=input_fields[8],
-                             foot_length=input_fields[9], ease=input_fields[10],)
-
-    return result
 
 @app.route('/links')
 def links():
@@ -77,45 +49,38 @@ def links():
 
 @app.route('/pattern')
 def pattern():
+    data, error_list = form_input(request)
 
-    input_fields = form_input(request)
-    units = input_fields[5]
-    stitches = input_fields[6]
-    rows = input_fields[7]
-    circum = input_fields[8]
-    foot_length = input_fields[9]
-    ease = input_fields[10]
-    error = input_fields[11]
-
-    if error == True:
-        url_values = {'units' : units,
-            'stitches' : stitches,
-            'rows' : rows,
-            'circum' : circum,
-            'foot_length' : foot_length,
-            'ease' : ease}
+    if error_list:
+        url_values = {
+            'units': data['units'],
+            'stitches': data['stitches'],
+            'rows': data['rows'],
+            'circum': data['circum'],
+            'foot_length': data['foot_length']['value'],
+            'ease': data['ease']['value'],
+        }
 
         url = '/?' + urlencode(url_values)
 
-        result = redirect(url)
+        return redirect(url)
 
-    else:
+    sock_info = sockgen.Pattern_info(
+        data['units'], data['stitches'], data['rows']
+    )
+    cast_on, heel_end, toe_end, heel_length = sockgen.pattern(
+        sock_info, data['circum']
+    )
 
-        sock_info = sockgen.Pattern_info(units, stitches, rows)
+    data.update({
+        'sock_info': sock_info,
+        'cast_on': cast_on,
+        'heel_end': heel_end,
+        'toe_end': toe_end,
+        'heel_length': heel_length,
+    })
+    return render_template('pattern.html', data=data)
 
-        pattern = sockgen.pattern(sock_info, circum, foot_length)
-
-        result = render_template('pattern.html',
-                                 st_color=input_fields[0], r_color=input_fields[1], c_color=input_fields[2],
-                                 fl_color=input_fields[3], e_color=input_fields[4],
-                                 units=units, stitches=stitches, rows=rows, circum=circum, foot_length=foot_length, ease=ease,
-                                 sock_info=sock_info,
-                                 cast_on = pattern[0],
-                                 heel_end = pattern[1],
-                                 toe_end = pattern[2],
-                                 heel_length = pattern[3],
-                                 )
-    return result
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 8000, debug=True)
--- sockgen.py
+++ sockgen.py
@@ -1,13 +1,10 @@
-__author__ = 'becky'
-
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#import argparse
+
 
 class Pattern_info(object):
 
-    def __init__(self, units = "inches", stitches=24, rows=30, ease=-8):
+    def __init__(self, units="inches", stitches=24, rows=30, ease=-8):
         self.units = units
         self.stitches = float(stitches)
         self.rows = float(rows)
@@ -22,21 +19,23 @@ class Pattern_info(object):
 
 def cast_on_stitches(pattern_info, foot_circum):
     """
-    Calculates number of stitches to provisionally cast on to start short row toe.
-    Ease should be entered as a percentage, negative or positive (usually negative for socks).
+    Calculates number of stitches to provisionally cast on to start short row
+    toe.
+    Ease should be entered as a percentage, negative or positive (usually
+    negative for socks).
     """
     sock_circum = float(foot_circum) + (float(foot_circum) * (pattern_info.ease/100))
     cast_on = round((float(sock_circum) * pattern_info.st_gauge) / 2)
     return cast_on
 
 
-
-
 def short_row_end_stitches(cast_on_sts, percent_end):
     """
-    Calcuates number of stitches at end of short row toe or heel; this number is even if the number of toe/heel stitches is even,
+    Calcuates number of stitches at end of short row toe or heel; this number
+    is even if the number of toe/heel stitches is even,
     and odd if the number of toe/heel stitches is odd.
-    percent_end is the percentage of stitches at the end of toe/heel (usually 40% for toe and 50% for heel)
+    percent_end is the percentage of stitches at the end of toe/heel (usually
+    40% for toe and 50% for heel)
     """
 
     x = round(cast_on_sts * (float(percent_end)/100))
@@ -59,14 +58,6 @@ def short_row_end_stitches(cast_on_sts, percent_end):
             return x
 
 
-def short_row_end_test():
-    print "Test short row end sts:"
-    toe_stitch_list = [24, 26, 28, 30, 32, 34, 36, 37]
-    for i in toe_stitch_list:
-        print "Toe stitches: ", i, "; toe end sts: ", short_row_end_stitches(int(i), 40)
-        print "Heel stitches: ", i, "; heel end sts: ", short_row_end_stitches(int(i), 50)
-
-
 def short_row_length(cast_on_sts, short_row_end_stitches, pattern_info):
     """
     Calculate length of the short row toe or heel.
@@ -76,17 +67,14 @@ def short_row_length(cast_on_sts, short_row_end_stitches, pattern_info):
     return sr_length
 
 
-
-def pattern(pattern_info, foot_circum, foot_length):
+def pattern(pattern_info, foot_circum):
 
     cast_on = cast_on_stitches(pattern_info, foot_circum)
     heel_end = short_row_end_stitches(cast_on, 50)
     toe_end = short_row_end_stitches(cast_on, 40)
     heel_length = short_row_length(cast_on, heel_end, pattern_info)
 
-    print cast_on, heel_end, toe_end, heel_length
     return cast_on, heel_end, toe_end, heel_length
-
 
 
 def main():
